6495ba437e6ff10a49bb75d7b8a427f16b61d711
Alexandre Garcia Simões <ale.gsimoes@gmail.com>
2021-08-16 14:44:26 -0300

    initial commit

---
+++ app/app.ts
@@ -0,0 +1,7 @@
+import { Utils } from './utils.js';
+
+const utils = new Utils();
+
+console.log('Validação de e-mail: ', utils.validaEmail());
+console.log('Valor com desconto(%):', utils.calculaDesconto());
+console.log('Juros simples: ', utils.calculaJurosSimples());
---
+++ dist/js/utils.js
@@ -0,0 +1,5 @@
+export class Utils {
+    validaEmail() { }
+    calculaDesconto() { }
+    calculaJurosSimples() { }
+}
